a0f6552e5f3e10341dc1eaf298d9673f52dac591
unstephenk <unstephenk@gmail.com>
2025-05-06 15:12:49 -0500

    first run

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Button from './Button'
+import useCount from './store'
+
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useCount();
 
   return (
     <>
@@ -16,18 +18,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Remote App</h1>
+        <Button />
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count: number) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
